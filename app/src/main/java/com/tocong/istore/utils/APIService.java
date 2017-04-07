7bfa56504c186210ce1c2ad13489b3e4a17a338f
Jialebao <Jialebao@taocong.local>
2017-04-07 15:16:45 +0800

    Initial commit

---
+++ app/src/main/java/com/tocong/istore/utils/APIService.java
@@ -0,0 +1,8 @@
+package com.tocong.istore.utils;
+
+/**
+ * Created by Jialebao on 2017/4/7.
+ */
+
+public class APIService {
+}
---
+++ app/src/main/java/com/tocong/istore/utils/httputils/RetrofitUtil.java
@@ -0,0 +1,8 @@
+package com.tocong.istore.utils.httputils;
+
+/**
+ * Created by Jialebao on 2017/4/7.
+ */
+
+public class RetrofitUtil {
+}
